7e40c6bc8cba78bd076f9c2c9b2ddc1bac4329f5
이강석 <65838532+cpfur18@users.noreply.github.com>
2024-06-25 13:42:52 +0900

    fix

--- src/main/java/com/multi/mini/movie/model/dto/MovieDTO.java
+++ src/main/java/com/multi/mini/movie/model/dto/MovieDTO.java
@@ -7,20 +7,21 @@ import java.sql.Timestamp;
 @Data
 public class MovieDTO {
 
-	private int movieNo; // 영화번호
-	private String movieTitle; // 제목
-	private String genreNo;
+	private int id;
+	private int movieNo;
+	private String movieTitle;
+	private int genreNo;
 	private int popularity;
-	private int duration; // 상영시간
+	private int duration;
 	private String originalLanguage;
 	private String movieContent;
 	private int adult;
 	private String posterPath;
 	private Timestamp createDate;
 	private Timestamp modifyDate;
-	
-	
-	
+
+
+
 
 
 
--- src/main/java/com/multi/mini/movie/model/mapper/MovieMapper.java
+++ src/main/java/com/multi/mini/movie/model/mapper/MovieMapper.java
@@ -1,41 +1,61 @@
 package com.multi.mini.movie.model.mapper;
 
-import com.multi.mini.movie.model.dto.CinemaDTO;
-import com.multi.mini.movie.model.dto.MovieDTO;
-import com.multi.mini.movie.model.dto.MovieScheduleDTO;
-import com.multi.mini.movie.model.dto.RegionDTO;
+import com.multi.mini.movie.model.dto.*;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 @Mapper
 public interface MovieMapper {
-	
-	
+
+
 	ArrayList<MovieDTO> findMovieList() throws Exception;
-	
+
 	ArrayList<RegionDTO> findRegionList() throws Exception;
-	
+
 	ArrayList<CinemaDTO> findCinemaListByRegionNo(int regionNo) throws Exception;
-	
+
 	ArrayList<MovieScheduleDTO> findScheduleListByCinemaNo(int cinemaNo) throws  Exception;
-	
+
 	MovieDTO findMovieByMovieNo(int movieNo) throws Exception;
 
+	ArrayList<SeatDTO> findAllSeat() throws Exception;
+
+
+	ArrayList<SeatDTO> findReservedSeat(MovieScheduleDTO movieScheduleDTO) throws Exception;
+
+	ArrayList<SeatDTO> findSeatListByScreen(String screenCode) throws Exception;
+
+	int insertReservation(ReservationDTO reservationDTO) throws Exception;
+
+	int findReservationNo(ReservationDTO reservationDTO) throws Exception;
+
+	ArrayList<VWResDataGroupDTO> findAllReservationByMemberNo(Map<String, Object> params) throws Exception;
+
+	int findAllReservationCountByMemberNo(int memberNO) throws Exception;
+
+	ArrayList<VWResDataGroupDTO> findAllNotPaidReservationByMemberNo(Map<String, Object> params);
+
+	int findAllNotPaidReservationCountByMemberNo(int memberNO) throws Exception;
+
+	ArrayList<Integer> findRsvNoListByVWGroupDTO(VWResDataGroupDTO vwResDataGroupDTO) throws Exception;
+
+	ArrayList<VWRewDataDTO> findVWReviewListByMovieNo(Map<String, Object> params) throws Exception;
+
+	int findAllReviewCountByMovieNo(int movieNo) throws Exception;
+
+	int insertReview(ReviewDTO reviewDTO) throws Exception;
 
 
 
+	ArrayList<VWMovieManageDataDTO> findAllEnrolledMovie(MoviePageDTO moviePageDTO) throws Exception;
 
+	int findAllEnrolledMovieCount() throws Exception;
 
+	void insertGenre(GenreDTO genreDTO) throws Exception;
 
-
-
-
-
-
-
-
-
+	void insertAPIMovie(MovieDTO movieDTO) throws Exception;
 
 	ArrayList<CinemaDTO> findCinemaList() throws Exception;
 }
