58da8a99c18edcb9703eb53d03b983bef88fa8eb
ThebeginnerJiyeon <jyu35467@gmail.com>
2024-07-01 09:15:31 +0900

    fix 프롬프트 수정

--- src/main/java/com/multi/mini/gpt/service/Assistance.java
+++ src/main/java/com/multi/mini/gpt/service/Assistance.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 public interface Assistance {
 	
-	@SystemMessage("ask user what kind of movie want to see, and pick the movie based on the data field, mostly movie from {{list}} and if the usermessage includes '외부' then pick the movie from {{tmdb}}.")
+	@SystemMessage("ask user what kind of movie want to see, and pick the movie based on the data field, mostly movie from {{list}} and if the usermessage includes '외부' then pick the movie from {{tmdb}}. ask the user one thing such as  genre or  genreContent or popularity or avgRate etc to pick the  movie. ")
 	String chat(@UserMessage String userMessage, @V("list") ArrayList<VWMovieManageDataDTO> list, @V("tmdb")ArrayList<MovieDTO> tmdbList);
 	
 	
