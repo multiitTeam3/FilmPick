f3601a237ff50afbd5cbfdc29778a5208a7194a5
ThebeginnerJiyeon <jyu35467@gmail.com>
2024-07-03 14:24:01 +0900

    1
    1

---
+++ src/main/java/com/multi/mini/point/model/dto/Point.java
@@ -0,0 +1,11 @@
+package com.multi.mini.point.model.dto;
+
+import lombok.Data;
+
+@Data
+public class Point {
+    private int pointTxNo;
+    private int memberNo;
+    private int pointChange;
+    private String pointDesc;
+}
---
+++ src/main/java/com/multi/mini/point/service/PointService.java
@@ -0,0 +1,4 @@
+package com.multi.mini.point.service;
+
+public interface PointService {
+}
