d607088b48ee690dde3879a2d7b06fd73875e423
이강석 <65838532+cpfur18@users.noreply.github.com>
2024-07-03 15:47:10 +0900

    1
    1

--- build/resources/main/static/js/tempPassword.js
+++
@@ -1,5 +0,0 @@
-function tempPassword() {
-    let options = "toolbar=no,scrollbars=no,resizable=yes,status=no,menubar=no,width=1200, height=800, top=0,left=0";
-    window.open("http://www.naver.com","_blank", options)
-
-}
